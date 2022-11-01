6f6d67651efea2089b12cdc4ecf6ca2e82af3597
LAPTOP-MCUS6VJL\12644 <126418522@qq.com>
2022-11-01 21:31:11 +0800

    初始化分类页面

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,22 +1,165 @@
 <template>
   <view>
-    分类
+    <view class="scroll-view-container">
+      <!-- 左侧的滚动视图区域 -->
+      <scroll-view class="left-scroll-view" :scroll-y="true" :style="{height:winHeight+ 'px'}">
+        <view class="left-scroll-view-item active">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">多复制一些节点，演示纵向滚动效果...</view>
+      </scroll-view>
+      <!-- 右侧的滚动视图区域 -->
+      <scroll-view class="right-scroll-view" scroll-y :style="{height: winHeight + 'px'}">
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">多复制一些节点，演示纵向滚动效果</view>
+      </scroll-view>
+    </view>
   </view>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
+<script setup>
+  import {
+    ref,
+    reactive,
+    toRefs,
+    onBeforeMount,
+    onMounted,
+    onUpdated,
+    onBeforeUpdate,
+    onBeforeUnmount,
+    onUnmounted,
+    computed,
+    watch,
+  } from 'vue'
 
-      }
-    },
-    methods: {
+  import {
+    onLoad,
+    onShow,
+    onPullDownRefresh,
+    onReachBottom,
+  } from '@dcloudio/uni-app'
 
-    }
+  const topPadding = ref('0')
+  const winHeight = ref('0')
+
+  const getHeight = () => {
+    const statusBarHeight = uni.getSystemInfoSync().statusBarHeight
+    console.log(statusBarHeight)
+    const customHeight = wx.getMenuButtonBoundingClientRect()
+    console.log(customHeight)
+    const navigationBarHeight = customHeight.height + (customHeight.top - statusBarHeight) * 2
+    console.log(navigationBarHeight)
+
+    topPadding.value = `${navigationBarHeight.value + statusBarHeight.value}px`
+    console.log(topPadding.value)
   }
+
+
+  onLoad(() => {
+    // 获取当前系统的信息
+    const sysInfo = uni.getSystemInfoSync()
+    console.log(sysInfo)
+    // 为窗口可用高度动态赋值
+    winHeight.value = sysInfo.windowHeight
+
+
+    // getHeight()
+  })
 </script>
 
 <style lang="scss">
+  .scroll-view-container {
+    display: flex;
 
+    .left-scroll-view {
+      width: 120px;
+
+      .left-scroll-view-item {
+        line-height: 60px;
+        background-color: #f7f7f7;
+        text-align: center;
+        font-size: 12px;
+
+        // 激活项的样式
+        &.active {
+          background-color: #ffffff;
+          position: relative;
+
+          // 渲染激活项左侧的红色指示边线
+          &::before {
+            content: ' ';
+            display: block;
+            width: 3px;
+            height: 30px;
+            background-color: #c00000;
+            position: absolute;
+            left: 0;
+            top: 50%;
+            transform: translateY(-50%);
+          }
+        }
+      }
+    }
+  }
 </style>
