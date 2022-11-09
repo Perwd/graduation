5194afab9c820a8e99c293b2ec641be0e50a2bf0
wlr <1264418522@qq.com>
2022-11-09 17:03:35 +0800

    搜索页面完结，增加清除方法

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -8,6 +8,7 @@
 <script setup lang="ts">
 	import {
 		ref,
+		reactive,
 		// toRefs,
 		// reactive
 	} from 'vue'
@@ -15,27 +16,72 @@
 		onLoad,
 	} from '@dcloudio/uni-app'
 
-	interface ValueObject {
-		value ? : Number
+	type ValueObject = {
+		value ? : Number,
+		query ? : String,
+		cid ? : String,
+		pageNum ? : Number,
+		pageSize ? : Number,
 	}
 	// 通过接口定义对象所具有的属性，
 	//通过接口声明对象后，所具有的属性值一目了然
 	const cid2: ValueObject = Object.create(null);
 
-	const cid = ref(String)
-	const cid3 = ref < string > ('s')
+	const cid = ref < String > ()
+	const goodsList = ref([])
+	const total = ref < Number > ()
+	// const cid3 = ref < string > ('s')
 
+	const queryObj: ValueObject = reactive({
+		query: '',
+		cid: '',
+		pageNum: 1,
+		pageSize: 10
+	})
 
 
 	const imgList = ref < string[] > ([]);
 
+
+	// function getGoodsList() {
+	// 	const {
+	// 		data: res
+	// 	} = await (uni as any).$http.get('/api/public/v1/goods/search', queryObj)
+	// 	console.log(1)
+	// 	console.log(res)
+	// 	console.log(res.meta)
+	// 	if (res.meta.status !== 200) return (uni as any).$showMsg()
+	// 	// 为数据赋值
+	// 	goodsList.value = res.message.goods
+	// 	total.value = res.message.total
+
+	// }
+
+	const getGood = async () => {
+		await (uni as any).$http.get('/api/public/v1/goods/search', queryObj.value).then((res: any) => {
+			console.log(2)
+			console.log(res)
+			if (res.data.meta.status !== 200) return (uni as any).$showMsg()
+			// 为数据赋值
+			goodsList.value = res.data.message.goods
+			total.value = res.data.message.total
+		})
+	}
 	onLoad((option: any) => {
+		console.log(1)
 		console.log(option)
 		cid.value = option.cid
 
-		cid2.value = 2
+
 		imgList.value = ['1']
-		cid3.value = 'q'
+
+
+		// queryObj.query = option.query || ''
+		// queryObj.cid = option.cid || ''
+		// console.log(queryObj)
+		// getGoodsList()
+		getGood()
+
 	})
 </script>
 
--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -1,216 +1,228 @@
 <template>
-  <view class="search-box">
+	<view class="search-box">
 
-    <uni-search-bar @input="input" focus placeholder="自定1" :radius="100" cancelButton="none">
-
-
-    </uni-search-bar>
-
-    <!-- 搜索建议列表 -->
-    <view class="sugg-list" v-if="data.searchResults.length!==0">
-      <view class="sugg-item" v-for="(item, i) in data.searchResults" :key="i" @click="gotoDetail(item.goods_id)">
-        <view class="goods-name">{{item.goods_name}}</view>
-        <uni-icons type="arrowright" size="16"></uni-icons>
-      </view>
-    </view>
-
-    <!-- 搜索历史 -->
-    <view class="history-box" v-else>
-      <!-- 标题区域 -->
-      <view class="history-title">
-        <text>搜索历史</text>
-        <uni-icons type="trash" size="17"></uni-icons>
-      </view>
-      <!-- 列表区域 -->
-      <view class="history-list">
-
-        <uni-tag inverted :text="item" v-for="(item, i) in historys" :key="i">1</uni-tag>
-      </view>
-    </view>
+		<uni-search-bar @input="input" focus placeholder="自定1" :radius="100" cancelButton="none">
 
 
-  </view>
+		</uni-search-bar>
+
+		<!-- 搜索建议列表 -->
+		<view class="sugg-list" v-if="data.searchResults.length!==0">
+			<view class="sugg-item" v-for="(item, i) in data.searchResults" :key="i" @click="gotoDetail(item.goods_id)">
+				<view class="goods-name">{{item.goods_name}}</view>
+				<uni-icons type="arrowright" size="16"></uni-icons>
+			</view>
+		</view>
+
+		<!-- 搜索历史 -->
+		<view class="history-box" v-else>
+			<!-- 标题区域 -->
+			<view class="history-title">
+				<text>搜索历史</text>
+				<uni-icons type="trash" size="17" @click="clean"></uni-icons>
+			</view>
+			<!-- 列表区域 -->
+			<view class="history-list">
+
+				<uni-tag @click="gotoGoodsList(item)" inverted :text="item" v-for="(item, i) in historys" :key="i">1
+				</uni-tag>
+			</view>
+		</view>
+
+
+	</view>
 
 </template>
 
 <script setup lang="ts">
-  import {
-    ref,
-    reactive,
-    // toRefs,
-    // onBeforeMount,
-    // onMounted,
-    // onUpdated,
-    // onBeforeUpdate,
-    // onBeforeUnmount,
-    // onUnmounted,
-    computed,
-    // watch,
-    nextTick
-  } from "vue";
-  import {
-    onLoad,
-    // onShow,
-    // onPullDownRefresh,
-    // onReachBottom,
-  } from "@dcloudio/uni-app";
+	import {
+		ref,
+		reactive,
+		// toRefs,
+		// onBeforeMount,
+		// onMounted,
+		// onUpdated,
+		// onBeforeUpdate,
+		// onBeforeUnmount,
+		// onUnmounted,
+		computed,
+		// watch,
+		nextTick
+	} from "vue";
+	import {
+		onLoad,
+		// onShow,
+		// onPullDownRefresh,
+		// onReachBottom,
+	} from "@dcloudio/uni-app";
 
-  type Data2 = {
-    timeout ? : any,
-    searchResults ? : Array < String >
-  }
+	type Data2 = {
+		timeout ? : any,
+		searchResults ? : Array < String >
+	}
 
-  const str = ref('')
+	const str = ref('')
 
-  const historyList = ref < String[] > (['a', 'app', 'apple'])
-  // const historyList = ref(['a', 'app', 'apple'])
-  const historys = computed(() => {
-    return [...historyList.value].reverse()
-  })
-  const data = reactive < Data2 > ({
-    timeout: null,
-    searchResults: []
-  });
-
-  const input = (e) => {
-    clearTimeout(data.timeout) //清楚定时器
-    data.timeout = setTimeout(() => {
-      console.log(e)
-      str.value = e
-
-      getSearchList()
-    }, 500);
-  }
+	const historyList = ref < String[] > (['a', 'app', 'apple'])
 
 
+	const historys = computed(() => {
+		return [...historyList.value].reverse()
+	})
 
+	const data = reactive < Data2 > ({
+		timeout: null,
+		searchResults: []
+	});
 
+	const input = (e) => {
+		clearTimeout(data.timeout) //清楚定时器
+		data.timeout = setTimeout(() => {
+			// console.log(e)
+			str.value = e
 
-
-  const getSearchList = async () => {
-    if (str.value === '') return data.searchResults = []
-    // 发起请求，获取搜索建议列表
-    const {
-      data: res
-    } = await (uni as any).$http.get('/api/public/v1/goods/qsearch', {
-      query: str.value
-    })
-    if (res.meta.status !== 200) return (uni as any).$showMsg()
-    data.searchResults = res.message
-    console.log(data.searchResults)
-
-    saveSearchHistory()
-  }
-
-
-  function gotoDetail(goods_id: string) {
-    uni.navigateTo({
-      // 指定详情页面的 URL 地址，并传递 goods_id 参数
-      url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods_id
-    })
-
-  }
-
-  // function saveSearchHistory() {
-  //   historyList.value.push(str.value)
-
-  // }
-
-  // 保存搜索关键词为历史记录 
-  function saveSearchHistory() {
-    // this.historyList.push(this.kw)
-
-    // 1. 将 Array 数组转化为 Set 对象
-    const set = new Set(historyList.value)
-    // 2. 调用 Set 对象的 delete 方法，移除对应的元素
-    set.delete(str.value)
-    // 3. 调用 Set 对象的 add 方法，向 Set 中添加元素
-    set.add(str.value)
-    // 4. 将 Set 对象转化为 Array 数组
-    historyList.value = Array.from(set)
-    uni.setStorageSync('str', JSON.stringify(historyList.value))
-  }
+			getSearchList()
+		}, 500);
+	}
 
 
 
 
-  // function timeOut() {
-  // }
-  // 异步使用
-  onLoad(async () => {
 
-    await nextTick()
-    console.log(historyList.value)
-    historyList.value = [JSON.stringify(uni.getStorageSync('str'))] || []
 
-    console.log(historyList.value)
-  })
+	const getSearchList = async () => {
+		if (str.value === '') return data.searchResults = []
+		// 发起请求，获取搜索建议列表
+		const {
+			data: res
+		} = await (uni as any).$http.get('/api/public/v1/goods/qsearch', {
+			query: str.value
+		})
+		if (res.meta.status !== 200) return (uni as any).$showMsg()
+		data.searchResults = res.message
+		// console.log(data.searchResults)
+
+		saveSearchHistory()
+	}
+
+
+	function gotoDetail(goods_id: Number) {
+		console.log(goods_id)
+		uni.navigateTo({
+			// 指定详情页面的 URL 地址，并传递 goods_id 参数
+			url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods_id
+		})
+
+	}
+
+	function gotoGoodsList(item) {
+		console.log(item)
+		uni.navigateTo({
+			// 指定详情页面的 URL 地址，并传递 goods_id 参数
+			url: '/subpkg/goods_list/goods_list?goods_id=' + item
+		})
+
+	}
+
+	// 保存搜索关键词为历史记录 
+	function saveSearchHistory() {
+		// this.historyList.push(this.kw)
+
+		// 1. 将 Array 数组转化为 Set 对象
+		const set = new Set(historyList.value)
+		// 2. 调用 Set 对象的 delete 方法，移除对应的元素
+		set.delete(str.value)
+		// 3. 调用 Set 对象的 add 方法，向 Set 中添加元素
+		set.add(str.value)
+		// 4. 将 Set 对象转化为 Array 数组
+		historyList.value = Array.from(set)
+		// console.log(historyList.value)
+		uni.setStorageSync('str', JSON.stringify(historyList.value))
+	}
+
+
+	function clean() {
+		historyList.value = []
+
+		uni.setStorageSync('str', '')
+	}
+
+
+	// 异步使用
+	onLoad(async () => {
+
+		await nextTick()
+		// console.log(historyList.value)
+		historyList.value = [JSON.stringify(uni.getStorageSync('str'))] || []
+
+		// console.log(historyList.value)
+	})
 </script>
 
 <style lang="scss">
-  .search-box {
-    position: sticky;
-    top: 0;
-    z-index: 999;
-  }
+	.search-box {
+		position: sticky;
+		top: 0;
+		z-index: 999;
+	}
 
-  .uni-searchbar {
-    // width: 100px;
-    // height: 100px;
-    /* #ifndef APP-NVUE */
-    display: flex;
-    /* #endif */
-    flex-direction: row;
-    position: relative;
-    padding: 16rpx;
-    /* 将默认的 #FFFFFF 改为 #C00000 */
-    background-color: #c00000;
-  }
+	.uni-searchbar {
+		// width: 100px;
+		// height: 100px;
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		position: relative;
+		padding: 16rpx;
+		/* 将默认的 #FFFFFF 改为 #C00000 */
+		background-color: #c00000;
+	}
 
-  .sugg-list {
-    padding: 0 5px;
+	.sugg-list {
+		padding: 0 5px;
 
-    .sugg-item {
-      font-size: 12px;
-      padding: 13px 0;
-      border-bottom: 1px solid #efefef;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
+		.sugg-item {
+			font-size: 12px;
+			padding: 13px 0;
+			border-bottom: 1px solid #efefef;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
 
-      .goods-name {
-        // 文字不允许换行（单行文本）
-        white-space: nowrap;
-        // 溢出部分隐藏
-        overflow: hidden;
-        // 文本溢出后，使用 ... 代替
-        text-overflow: ellipsis;
-        margin-right: 3px;
-      }
-    }
-  }
+			.goods-name {
+				// 文字不允许换行（单行文本）
+				white-space: nowrap;
+				// 溢出部分隐藏
+				overflow: hidden;
+				// 文本溢出后，使用 ... 代替
+				text-overflow: ellipsis;
+				margin-right: 3px;
+			}
+		}
+	}
 
-  .history-box {
-    padding: 0 5px;
+	.history-box {
+		padding: 0 5px;
 
-    .history-title {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      height: 40px;
-      font-size: 13px;
-      border-bottom: 1px solid #efefef;
-    }
+		.history-title {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			height: 40px;
+			font-size: 13px;
+			border-bottom: 1px solid #efefef;
+		}
 
-    .history-list {
-      display: flex;
-      flex-wrap: wrap;
+		.history-list {
+			display: flex;
+			flex-wrap: wrap;
 
-      .uni-tag {
+			.uni-tag {
 
-        margin-top: 5px;
-        margin-right: 5px;
-      }
-    }
-  }
+				margin-top: 5px;
+				margin-right: 5px;
+			}
+		}
+	}
 </style>
