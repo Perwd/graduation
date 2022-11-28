9e5c84fdd7b5166053259ac59e581f12ea58ff9f
LAPTOP-MCUS6VJL\12644 <126418522@qq.com>
2022-11-28 21:29:24 +0800

    优化购物车角标显示

--- hook/useBadge.ts
+++ hook/useBadge.ts
@@ -1,36 +1,42 @@
 //useCounter.js
 import {
-	// ref,
-	computed
+  // ref,
+  computed
 } from "vue";
 import {
-	useCounterStore
+  useCounterStore
 } from '../pinia/cart';
 import {
-	storeToRefs
+  storeToRefs
 } from 'pinia'
 
 export default function() {
-	const double = computed(() => Number(count) * 2)
+  const double = computed(() => Number(count) * 2)
 
-	const store = useCounterStore();
-	const {
-		count,
-		total
-	} = storeToRefs(store)
+  const store = useCounterStore();
+  const {
+    count,
+    total
+  } = storeToRefs(store)
 
-	function setBadge() {
-		// console.log('total')
-		// console.log(total.value)
-		// 调用 uni.setTabBarBadge() 方法，为购物车设置右上角的徽标
-		uni.setTabBarBadge({
-			index: 2, // 索引
-			text: `${total.value}` // 注意：text 的值必须是字符串，不能是数字
-		})
-	}
-	return {
-		count,
-		double,
-		setBadge
-	}
+  function setBadge() {
+    // console.log('total')
+    // console.log(total.value)
+
+    if (typeof total.value === 'number' && total.value !== 0) {
+
+      // 调用 uni.setTabBarBadge() 方法，为购物车设置右上角的徽标
+      uni.setTabBarBadge({
+        index: 2, // 索引
+        // 注意：text 的值必须是字符串，不能是数字
+        text: `${total.value}`
+      })
+    }
+
+  }
+  return {
+    count,
+    double,
+    setBadge
+  }
 }
--- pinia/cart.ts
+++ pinia/cart.ts
@@ -3,12 +3,12 @@ import { defineStore } from 'pinia';
 import { TestType } from './store/type';
 
 type Cart = {
-	goods_id: string,
-	goods_name: string,
-	goods_price: number,
-	goods_count: number,
-	goods_small_logo: string,
-	goods_state: Boolean,
+  goods_id: string,
+  goods_name: string,
+  goods_price: number,
+  goods_count: number,
+  goods_small_logo: string,
+  goods_state: Boolean,
 }
 // 接口继承
 // interface StateType extends TestType {
@@ -21,99 +21,99 @@ type Cart = {
 // }
 
 type StateType = TestType & {
-	cart: Cart[],
-	count: number,
-	type: string,
-	// name:TestType ,
-	// id: TestType,
-	row?: object
+  cart: Cart[],
+  count: number,
+  type: string,
+  // name:TestType ,
+  // id: TestType,
+  row?: object
 }
 
 export const useCounterStore =
-	// defineStore('cart', {  方式一
-	defineStore({
-		id: 'cart',
-		state: () => {
-			return {
-				count: 0,
-				type: 'wxxcx',
-				name: 'wlr',
-				id: "000000",
-				row: {},
-				// 购物车的数组，用来存储购物车中每个商品的信息对象
-				// 每个商品的信息对象，都包含如下 6 个属性：
-				// { goods_id, goods_name, goods_price, goods_count, goods_small_logo, goods_state }
-				// cart: new Array<Cart>(),
-				cart: JSON.parse(uni.getStorageSync('cart') || new Array<Cart>()),
-			};
-		},
-		// 也可以这样定义
-		// state: () => ({ count: 0 })
-		actions: {
-			increment() {
-				this.count++;
-			},
-			// 2秒后更新状态
-			updateUid(context: any, payload: any) {
-				setTimeout(() => {
-					context.commit('updateUid', payload)
-				}, 2000)
-			},
-			apiGetUid(context: any) {
-				console.log('vuex', context)
+  // defineStore('cart', {  方式一
+  defineStore({
+    id: 'cart',
+    state: () => {
+      return {
+        count: 0,
+        type: 'wxxcx',
+        name: 'wlr',
+        id: "000000",
+        row: {},
+        cart: new Array<Cart>(),
+        // cart: JSON.parse(uni.getStorageSync('cart') || new Array<Cart>()),
+        // cart: JSON.parse(uni.getStorageSync('cart') ) || new Array<Cart>(),
+        // cart: JSON.parse(uni.getStorageSync('cart' || [])) || new Array<Cart>(),
 
-				this.count++
-				console.log(this.count)
-				// console.log(this.type)
-				// console.log(this.name)
-			},
-			addToCart(goods: Cart) {
+      };
+    },
+    // 也可以这样定义
+    // state: () => ({ count: 0 })
+    actions: {
+      increment() {
+        this.count++;
+      },
+      // 2秒后更新状态
+      updateUid(context: any, payload: any) {
+        setTimeout(() => {
+          context.commit('updateUid', payload)
+        }, 2000)
+      },
+      apiGetUid(context: any) {
+        console.log('vuex', context)
 
-				const findResult = this.cart.find((x: Cart) => x.goods_id === goods.goods_id)
+        this.count++
+        console.log(this.count)
+        // console.log(this.type)
+        // console.log(this.name)
+      },
+      addToCart(goods: Cart) {
 
-				if (!findResult) {
-					// 没有这件商品，则直接 push
-					this.cart.push(goods)
+        const findResult = this.cart.find((x: Cart) => x.goods_id === goods.goods_id)
 
-				} else {
-					// 有这件商品，增加
-					findResult.goods_count++
-				}
+        if (!findResult) {
+          // 没有这件商品，则直接 push
+          this.cart.push(goods)
 
-
-				console.log('触发cart')
-				console.log(this.cart)
-				this.saveToStorage()
-			},
-			// 将购物车中的数据持久化存储到本地
-			saveToStorage() {
-				console.log('数据同步存入本地')
-				uni.setStorageSync('cart', JSON.stringify(this.cart))
-			},
-			// 更新购物车中商品的勾选状态
-			updateGoodsState(goods: any) {
-				// 根据 goods_id 查询购物车中对应商品的信息对象
-				const findResult = this.cart.find((x: Cart) => x.goods_id === goods.goods_id)
-
-				// 有对应的商品信息对象
-				if (findResult) {
-					// 更新对应商品的勾选状态
-					findResult.goods_state = goods.goods_state
-					// 持久化存储到本地
-					this.saveToStorage()
-
-					// 课程中 this.commit('m_cart/saveToStorage')，就是调用方法saveToStorage
-				}
-			}
+        } else {
+          // 有这件商品，增加
+          findResult.goods_count++
+        }
 
 
-		},
-		getters: {
-			total: (state) => {
-				let num = 0
-				state.cart.forEach(goods => num += goods.goods_count)
-				return num as number
-			},
-		},
+        console.log('触发cart')
+        console.log(this.cart)
+        this.saveToStorage()
+      },
+      // 将购物车中的数据持久化存储到本地
+      saveToStorage() {
+        console.log('数据同步存入本地')
+        uni.setStorageSync('cart', JSON.stringify(this.cart))
+      },
+      // 更新购物车中商品的勾选状态
+      updateGoodsState(goods: any) {
+        // 根据 goods_id 查询购物车中对应商品的信息对象
+        const findResult = this.cart.find((x: Cart) => x.goods_id === goods.goods_id)
 
-	});
+        // 有对应的商品信息对象
+        if (findResult) {
+          // 更新对应商品的勾选状态
+          findResult.goods_state = goods.goods_state
+          // 持久化存储到本地
+          this.saveToStorage()
+
+          // 课程中 this.commit('m_cart/saveToStorage')，就是调用方法saveToStorage
+        }
+      }
+
+
+    },
+    getters: {
+      total: (state: any) => {
+        let num = 0
+        state.cart.forEach((goods: any) => num += goods.goods_count)
+        return num
+      },
+    },
+
+  });
